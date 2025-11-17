0e80aa19fb769a57e215a4de6c40c1296ff7beab
ivper <ivperezorbaneja@gmail.com>
2025-11-17 17:28:49 +0100

    primer cambio del proyecto

---
+++ src/main/java/es/daw/foodexpressmvc/service/RestaurantService.java
@@ -0,0 +1,17 @@
+package es.daw.foodexpressmvc.service;
+
+
+import es.daw.foodexpressmvc.dto.RestaurantDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RestaurantService {
+
+    public List<RestaurantDTO> getRestaurants() {
+
+        // Pendiente de usar WebClient para hacer la peticion al endpoint GET
+
+    }
+}
